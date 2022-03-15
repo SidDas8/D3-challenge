dbe44a67210f5936c12de0972a55fae06efb4c28
SidDas8 <siddharthdas168@gmail.com>
2022-03-14 21:54:17 -0700

    Working on app.js. Set SVG dimensions and margins.

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+// Step 1: Set up our chart //
+
+// SVG Dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Margins
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
